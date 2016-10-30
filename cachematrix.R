a4347a6fcf16c31f47c5eb2c5bb5407f6c28b617
Flavio Mauro <flaviohcmauro@gmail.com>
2016-10-30 15:08:00 -0200

    Flavio H C Mauro cacheMatrix.R Coursera Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse 
+##      of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion 
+##      that we will not discuss here). Your assignment is to write a pair of functions that cache the 
+##      inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMat <- NULL
+    setMat <- function(y) {
+        x <<- y
+        invMat <<- NULL
+    }
+    getMat <- function() x
+    setInvMat <- function(inverse) invMat <<- inverse
+    getInvMat <- function() invMat
+    list(setMat=setMat, getMat=getMat, setInvMat=setInvMat, getInvMat=getInvMat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##      If the inverse has already been calculated (and the matrix has not changed), 
+##      then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInvMat()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$getMat()
+    inv <- solve(data)
+    x$setInvMat(inv)
+    inv
 }
+
+## Define a square inversible matrix
+x = matrix(c(1, 5, 6, 7, 8, 3, 2, 5, 7), nrow = 3, ncol = 3)
+
+## Set Cache Matrix to m
+m <- makeCacheMatrix(x)
+
+## Set Matrix to upper environment
+m$setMat(x)
+
+## Get Matrix from upper environment
+m$getMat()
+
+## inverse Matrix from upper envionement
+cacheSolve(m)
+
+
+## Validation that cacheSolve shows the same data as m$getInvMat()
+m$getInvMat()
+
